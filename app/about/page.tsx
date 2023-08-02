768f849ab4ec00a0f77c10e024d8a2c3209c7197
jason_genobiagon <jason.genobiagon@fidx.co.jp>
2023-08-02 11:00:37 +0800

    add page header

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import AboutUs from '@/components/about/About'
+import PageHeader from '@/components/PageHeader';
 
 const About = () => {
+  const pageName = [
+    'Home',
+    'Pages',
+    'About Us'
+  ];
+
   return (
+    <>
+    <PageHeader pageName={pageName}></PageHeader>
    <AboutUs></AboutUs>
+   </>
   )
 }
 
