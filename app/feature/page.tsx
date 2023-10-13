ce9e54ad48e54f27fe62a432569b88495678aa86
jason_genobiagon <jason.genobiagon@fidx.co.jp>
2023-10-13 09:28:18 +0800

    fix feature right

--- app/feature/page.tsx
+++ app/feature/page.tsx
@@ -16,20 +16,25 @@ const FeaturePage = () =>{
             <PageHeader pageName={pageName}></PageHeader>
             <div className="container-xxl py-5">
                 <div className="container">
-                    <ReactWOW animation="fadeInUp">
-                    <div className="col-lg-6 animate__animated animate__fadeInUp" data-wow-delay="0.1s">
-                        <div className="position-relative me-lg-4">
-                            <img className="img-fluid w-100" src="img/feature.jpg" alt="" />
-                            <span
-                                className="position-absolute top-50 start-100 translate-middle bg-white rounded-circle d-none d-lg-block"
-                                style={{width: '120px', height: '120px'}}></span>
-                            <button type="button" className="btn-play" data-bs-toggle="modal"
-                                data-src="https://www.youtube.com/embed/DWRcNpR6Kdc" data-bs-target="#videoModal">
-                                <span></span>
-                            </button>
+                    <div className="row g-5 align-items-center">
+                        <ReactWOW animation="fadeInUp">
+                        <div className="col-lg-6 animate__animated animate__fadeInUp" data-wow-delay="0.1s">
+                            <div className="position-relative me-lg-4">
+                                <img className="img-fluid w-100" src="img/feature.jpg" alt="" />
+                                <span
+                                    className="position-absolute top-50 start-100 translate-middle bg-white rounded-circle d-none d-lg-block"
+                                    style={{width: '120px', height: '120px'}}></span>
+                                <button type="button" className="btn-play" data-bs-toggle="modal"
+                                    data-src="https://www.youtube.com/embed/DWRcNpR6Kdc" data-bs-target="#videoModal">
+                                    <span></span>
+                                </button>
+                            </div>
+                        </div>
+                        </ReactWOW>
+                        <div className="col-lg-6 animate__animated animate__fadeInUp" data-wow-delay="0.5s">
+                            <FeatureRight />
                         </div>
                     </div>
-                    </ReactWOW>
                 </div>
             </div>
         </>
